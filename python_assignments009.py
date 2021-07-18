7d57420fb443b27a03fdb06fc9deb3ec486a500f
burak <scientistburak@gmail.com>
2021-07-18 16:54:02 +0300

    1

---
+++ python_assignments009.py
@@ -0,0 +1,6 @@
+a = input("Enter a name: ").strip()
+if a == "Burak":
+    print("Hello Burak! Your Password is 123456")
+else:
+    print("Hello ", a, "See You Later.")
+# https://github.com/burakfly/python-assignments.git
